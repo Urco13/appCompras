50c77b48c04498d2b016be787c98d5c59be89930
Carlos Fernandez <calos.fernandez.external@atos.net>
2019-02-27 13:14:54 +0100

    045 ValueChanges

--- src/app/presupuestos/addpres/addpres.component.ts
+++ src/app/presupuestos/addpres/addpres.component.ts
@@ -11,6 +11,10 @@ export class AddpresComponent implements OnInit {
 
   presupuestoForm: FormGroup;
   presupuesto: any;
+  base: any;
+  tipo: any;
+  iva: any = 0;
+  total: any = 0;
 
   constructor(private pf: FormBuilder) { }
 
@@ -21,15 +25,26 @@ export class AddpresComponent implements OnInit {
       concepto: ['', [Validators.required, Validators.minLength(10)]],
       base: ['', Validators.required],
       tipo: ['', Validators.required],
-      iva: ['', Validators.required],
-      total: ['', Validators.required]
+      iva: this.iva,
+      total: this.total
     });
+
+
+    this.onChanges();
   }
 
   onSubmit(){
     this.presupuesto = this.savePresupuesto();
   }
 
+  onChanges(): void {
+    this.presupuestoForm.valueChanges.subscribe(valor => {
+      this.base = valor.base;
+      this.tipo = valor.tipo;
+      this.presupuestoForm.value.iva = this.base * this.tipo;
+      this.presupuestoForm.value.total = this.base + (this.base * this.tipo);
+    })
+  }
   savePresupuesto(){
     const savePresupuesto = {
       proveedor: this.presupuestoForm.get('proveedor').value,
