108de74e30155e2d174c4b741d145a0eb7300dfc
Carlos Fernández <carlosrfernandezgarcia@gmail.com>
2019-03-11 00:38:44 +0100

    056 inicio sesion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { PresupuestosComponent } from './presupuestos/presupuestos/presupuestos.
 import { EditpresComponent } from './presupuestos/editpres/editpres.component';
 import { RegistroComponent } from './autenticacion/registro/registro.component';
 import { AutenticacionService } from './servicios/autenticacion.service';
+import { InisesComponent } from './autenticacion/inises/inises.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -27,6 +28,7 @@ const routes: Routes = [
   { path: 'presupuestos', component: PresupuestosComponent },
   { path: 'editpres/:id', component: EditpresComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: 'inises', component: InisesComponent },
   { path: '**', component: InicioComponent }
 
 ]
@@ -42,6 +44,7 @@ const routes: Routes = [
     AddpresComponent,
     EditpresComponent,
     RegistroComponent,
+    InisesComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/servicios/autenticacion.service.ts
+++ src/app/servicios/autenticacion.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacionService {
 
-  constructor() {
+  constructor(private router: Router, private activatdRouter: ActivatedRoute) {
   }
 
   registroUsuario(userdata) {
     firebase.auth().createUserWithEmailAndPassword(userdata.email, userdata.password)
     .catch(error => { console.log(error);
+    });
+  }
+
+  inicioSesion(userdata){
+    firebase.auth().signInWithEmailAndPassword(userdata.email, userdata.password)
+    .then( response => {
+      console.log(response);
+      this.router.navigate(['/inicio']);
+    }).catch(error => {console.log(error);
     });
   }
 }
