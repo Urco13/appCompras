83b7b0f2ca6d90fc1e0b3b3971fe1c9846fd0b1b
Carlos Fernandez <calos.fernandez.external@atos.net>
2019-03-14 14:16:36 +0100

    062 addFra

--- src/app/facturas/facturas/addfra/addfra.component.ts
+++ src/app/facturas/facturas/addfra/addfra.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { FacturasService } from '../../facturas.service';
 
 @Component({
   selector: 'app-addfra',
@@ -10,9 +10,61 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 export class AddfraComponent implements OnInit {
 
   facturaForm: FormGroup;
-  constructor() { }
+  factura: any;
+  base: any;
+  tipo: any;
+  iva: any = 0;
+  total: any = 0;
+
+  constructor(private formBuilder: FormBuilder, private facturaService: FacturasService) { }
 
   ngOnInit() {
+
+    this.facturaForm = this.formBuilder.group({
+      proveedor: ['', Validators.required ],
+      fecha: ['', Validators.required ],
+      concepto: ['', Validators.required, Validators.minLength(10) ],
+      base: ['', Validators.required ],
+      tipo: ['', Validators.required ],
+      iva: this.iva,
+      total: this.total
+    });
+
+  this.onChanges();
   }
 
+  onChanges(): void {
+
+    this.facturaForm.valueChanges.subscribe(valor => {
+      this.base = valor.base;
+      this.tipo = valor.tipo;
+      this.facturaForm.value.iva = this.base * this.tipo;
+      this.facturaForm.value.total = this.base + (this.base * this.tipo);
+    });
+  }
+
+
+  onSubmit() {
+    this.factura = this.saveFactura();
+    this.facturaService.postFactura( this.factura )
+        .subscribe(newfra => {
+        })
+    this.facturaForm.reset();
+  }
+
+  saveFactura() {
+
+    const saveFactura = {
+
+      proveedor: this.facturaForm.get('proveedor').value,
+      fecha: this.facturaForm.get('fecha').value,
+      concepto: this.facturaForm.get('concepto').value,
+      base: this.facturaForm.get('base').value,
+      tipo: this.facturaForm.get('tipo').value,
+      iva: this.facturaForm.get('iva').value,
+      total: this.facturaForm.get('total').value
+
+    };
+    return saveFactura;
+  }
 }
